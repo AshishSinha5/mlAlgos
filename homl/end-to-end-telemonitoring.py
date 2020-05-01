003f691e92c7978626de0b2cb3533a164434159d
AshishSinha5 <ashish97.kumar@gmail.com>
2020-05-01 21:14:08 +0530

    Added notebook for homl chap 1

---
+++ homl/end-to-end-telemonitoring.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 22:55:17 2020
+
+@author: ashish
+"""
+
+import numpy as np
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/telemonitoring/parkinsons_updrs.data")
